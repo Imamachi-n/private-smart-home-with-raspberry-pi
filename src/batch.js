32b7e25aaf0df36a2f2bfd243e74cd2730813ece
Imamachi-n <naoto.imamachi@gmail.com>
2020-02-13 10:52:27 +0900

    fixed the bug: change from turn off data to turn on data

--- src/batch.js
+++ src/batch.js
@@ -2,7 +2,7 @@ const { redCmd } = require("./redCommand");
 
 // turn on my light
 try {
-  redCmd("/home/imamachi/private-smart-home-with-raspberry-pi/data/ch5.data");
+  redCmd("/home/imamachi/private-smart-home-with-raspberry-pi/data/ch4.data");
   console.log(
     "実行時刻: " + new Date().toLocaleString({ timeZone: "Asia/Tokyo" })
   );
