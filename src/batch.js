04762e0efa4b8493986fadcbfc5a53ac078cd0e4
Imamachi-n <naoto.imamachi@gmail.com>
2020-02-13 10:49:49 +0900

    added the timestamp batch script is executed

--- src/batch.js
+++ src/batch.js
@@ -1,4 +1,12 @@
 const { redCmd } = require("./redCommand");
 
 // turn on my light
-redCmd("/home/imamachi/private-smart-home-with-raspberry-pi/data/ch5.data");
+try {
+  redCmd("/home/imamachi/private-smart-home-with-raspberry-pi/data/ch5.data");
+  console.log(
+    "実行時刻: " + new Date().toLocaleString({ timeZone: "Asia/Tokyo" })
+  );
+  console.log("Successfully Done!!");
+} catch (err) {
+  console.error("ERROR: " + err);
+}
